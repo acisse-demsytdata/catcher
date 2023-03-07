bfe08fd928e6bdce4bc5ad7dbb3ae48f566d1846
Amadou Cisse <100707447+acisse-demsytdata@users.noreply.github.com>
2023-03-07 14:47:11 +0100

    Update catcher_utils.dart

--- lib/utils/catcher_utils.dart
+++ lib/utils/catcher_utils.dart
@@ -1,20 +1,7 @@
-import 'package:universal_io/io.dart';
-
 import 'package:flutter/cupertino.dart';
 
 class CatcherUtils {
-  static Future<bool> isInternetConnectionAvailable() async {
-    try {
-      final result = await InternetAddress.lookup('google.com');
-      if (result.isNotEmpty == true && result[0].rawAddress.isNotEmpty) {
-        return Future.value(true);
-      } else {
-        return Future.value(false);
-      }
-    } catch (exception) {
-      return Future.value(false);
-    }
-  }
+  static Future<bool> isInternetConnectionAvailable() async => true;
 
   static bool isCupertinoAppAncestor(BuildContext context) {
     return context.findAncestorWidgetOfExactType<CupertinoApp>() != null;
